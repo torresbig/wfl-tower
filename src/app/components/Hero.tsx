9dae4134be9ff9627c211a877e46162f0a71648e
torresbig <Thomas.schoenborn@t-online.de>
2026-04-12 17:16:46 +0200

    scrool test

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -140,7 +140,7 @@ export function Hero() {
         </a>
       </div>
 
-      <div className="flex flex-col items-center gap-3 pb-8">
+      <div className="flex flex-col items-center gap-8 pb-8">
         <span className="text-sm uppercase tracking-[0.4em] text-white/70">Weiter scrollen</span>
         <div className="flex space-x-2 animate-bounce">
             {[...Array(3)].map((_, index) => (
